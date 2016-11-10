34b522069c374c3768f19d76c9f19c2017c48f32
Orce Marinkovski <orce00@gmail.com>
2016-11-10 21:34:14 +0100

    init

---
+++ src/StringD.cpp
@@ -0,0 +1,101 @@
+/*
+ * StringD.cpp
+ *
+ *  Created on: Nov 5, 2016
+ *      Author: orce
+ */
+#include <StringD.h>
+#include <Arduino.h>
+
+#include <RemoteDebug.h>        //https://github.com/JoaoLopesF/RemoteDebug
+extern RemoteDebug Debug;
+
+
+StringD::StringD(char* StringToParase, const char* Delimiter){
+	StS=StringToParase;
+	delimiter=Delimiter;
+	pos_del=NULL;
+	Process();
+}
+
+
+/**
+ * @brief Private function that is launched by constructor. Function performs an analysis of the String
+ */
+void StringD::Process(){
+
+	//determine the number of nodes
+	unsigned int pos=1;
+	unsigned int prev_pos=0;
+
+	nodes=0; // initialise number of nodes
+	while(StS.indexOf(delimiter,pos)!=-1){
+		if (pos>prev_pos){
+			// there one more node increment variable of number of nodes
+			nodes++;
+			prev_pos=pos; // memorise the current position of the delimiter
+
+			Debug.println(StS.indexOf(delimiter,pos));
+
+			pos=StS.indexOf(delimiter,pos)+1;//move start position for next search
+		}
+		yield();
+	}
+	Debug.printf("Number of nodes %d\r\n",nodes);
+
+	// prepare an array with the number of delimters and their position in the string
+	Debug.printf("Heap before free %d\r\n",ESP.getFreeHeap());
+	if(sizeof(pos_del)>0){free(pos_del);}
+	Debug.printf("Heap before after free %d\r\n",ESP.getFreeHeap());
+	int *pos_del=(int*) malloc(nodes*sizeof(int));
+	Debug.printf("Heap after malloc %d\r\n",ESP.getFreeHeap());
+	Debug.printf("Size of pointer %d\r\n",sizeof(pos_del));
+//	int pos_del=(int*) malloc(nodes*sizeof(int));
+
+	// memorise the positions of the delimiters
+	pos=1;
+	prev_pos=0;
+	nodes=0; // initialise number of nodes
+
+	while(StS.indexOf(delimiter,pos)!=-1){
+		if (pos>prev_pos){
+			// there one more node increment variable of number of nodes
+
+			prev_pos=pos; // memorise the current position of the delimiter
+
+			Debug.println(StS.indexOf(delimiter,pos));
+
+			pos_del[nodes]=StS.indexOf(delimiter,pos);//move start position for next search
+			pos=pos_del[nodes]+1;
+
+			nodes++;
+		}
+		yield();
+	}
+
+	Debug.printf("Number of nodes %d\r\n",nodes);
+
+	Debug.printf("Pointer address inside class of nodes var %x\r\n",&nodes);
+
+
+}
+
+
+StringD::~StringD(){
+	//delete [] pos_del;
+}
+
+void StringD::PrintPosDelimiters(){
+
+}
+
+int StringD::getnodes(){
+	Debug.printf("Number of nodes in class function getnodes %d\r\n",nodes);
+
+	return nodes;
+}
+
+//
+//String StringD::Node(int position){
+//
+//}
---
+++ src/StringD.h
@@ -0,0 +1,38 @@
+/*
+ * StringD.h
+ *
+ *  Created on: Nov 5, 2016
+ *      Author: orce
+ */
+
+#ifndef LIB_STRINGD_SRC_STRINGD_H_
+#define LIB_STRINGD_SRC_STRINGD_H_
+
+
+
+#include "WString.h"
+
+
+class StringD:public String {
+	public:
+		String delimiter; // node delimiter for split up of string content
+		//StringD();
+
+		StringD(char* StringToParase, const char* Delimiter);
+
+		~StringD(); // destructor
+		//String Node(int position); // pointer to the
+		void PrintPosDelimiters(); // print the positions of the delimiters
+		int nodes; // number of nodes in the string
+		int *pos_del; //pointer for array of integer numbers. will be allocated at runtime
+		int getnodes();
+
+	private:
+		int StartPos; // start position of the node in the string
+		int EndPos; // end position of the node in the string
+		void Process(); // analyse String content
+		String StS; // copy of String content to be analysed
+
+};
+
+#endif /* LIB_STRINGD_SRC_STRINGD_H_ */
