0c4765e9835217dcecc06e2fe5ae7737845d802a
Orce Marinkovski <orce00@gmail.com>
2016-11-12 08:44:41 +0100

    description change

--- src/StringD.h
+++ src/StringD.h
@@ -24,7 +24,7 @@ class StringD:public String {
 		//String Node(int position); // pointer to the
 		void PrintPosDelimiters(); // print the positions of the delimiters
 		int m_nodes; // number of nodes in the string
-		uint8_t m_pos_del[254]; //pointer for array of integer numbers. will be allocated at runtime
+		uint8_t m_pos_del[254]; //array of delimiter positions in the string
 		uint8_t max_node(); //getter function for the total number of nodes identified
 		String get_node(uint8_t node);// function to get a specific node
 
