2ce25f8e426c98091e02bd0b775d7f6e30d83d97
Orce Marinkovski <orce00@gmail.com>
2016-11-20 18:36:21 +0100

    get  node stores node to class string NodeSelected

--- src/StringD.cpp
+++ src/StringD.cpp
@@ -18,7 +18,7 @@ StringD::StringD(char* StringToParase, const char* Delimiter){
 	m_delimiter=Delimiter;
 
 	#ifdef Debug_StringD
-	ThisDebug.printf("length of StS  = %d with content %s\r\n",StS.length(),StS.c_str());
+	ThisDebug.printf("C:length of StS  = %d with content %s\r\n",StS.length(),StS.c_str());
 	#endif
 	//m_pos_del=NULL;
 	Process();
@@ -31,12 +31,12 @@ StringD::StringD(char* StringToParase, const char* Delimiter){
  * @param length  position to insert a null terminated string in the
  */
 StringD::StringD(void* StringToParase, const char* Delimiter,int length){
-	memset((char*)StringToParase+length,0,1); // set the null character
+	memset((char*)StringToParase+length,0,1); // set the null character at the end og the length
 	StS=(char*)StringToParase;
 	m_delimiter=Delimiter;
 
 	#ifdef Debug_StringD
-	ThisDebug.printf("length of StS  = %d with content %s\r\n",StS.length(),StS.c_str());
+	ThisDebug.printf("C:length of StS  = %d with content %s\r\n",StS.length(),StS.c_str());
 	#endif
 	//m_pos_del=NULL;
 	Process();
@@ -48,6 +48,9 @@ StringD::StringD(void* StringToParase, const char* Delimiter,int length){
  * @brief Private function that is launched by constructor. Function performs an analysis of the String
  */
 void StringD::Process(){
+	#ifdef Debug_StringD
+	ThisDebug.printf("[%s]C:length of StS  = %d with content %s\r\n",__func__,StS.length(),StS.c_str());
+	#endif
 
 	//first while loop to determine the number of m_nodes
 	unsigned int pos=1;
@@ -88,8 +91,11 @@ void StringD::Process(){
 	}
 	m_nodes++; // increment node by one for the last node
 	#ifdef Debug_StringD
-		ThisDebug.printf("Number of m_nodes %d\r\n",m_nodes);
-		Debug.printf("Pointer address inside class of m_nodes var %x\r\n",&m_nodes);
+		ThisDebug.printf("C:Number of m_nodes %d\r\n",m_nodes);
+		int i;
+		for (i=1;i<(m_nodes+1);i++){
+			ThisDebug.printf("C:Node %d with content %s\r\n",i, get_node(i));
+		}
 	#endif
 }
 
@@ -98,18 +104,16 @@ StringD::~StringD(){
 	//delete [] m_pos_del;
 }
 
-void StringD::PrintPosDelimiters(){
 
-}
 
 uint8_t StringD::max_node(){
 	#ifdef Debug_StringD
-		ThisDebug.printf("Number of m_nodes in class function getnodes %d\r\n",m_nodes);
+		ThisDebug.printf("C:Number of m_nodes in class function getnodes %d\r\n",m_nodes);
 	#endif
 	return m_nodes;
 }
 
-char* StringD::get_node(uint8_t node){
+const char* StringD::get_node(uint8_t node){
 
 	if (node<1 | node > m_nodes){
 		return (char*) "Invalid_Node"; // return node selected is out of boundaries
@@ -119,20 +123,22 @@ char* StringD::get_node(uint8_t node){
 
 	uint8_t right=m_pos_del[node-1]; //set right boundary for node extract
 
+
 	if(node>1){
 		//set left boudary for node extract with an exception, always 0 for 1st node in string
 		left=m_pos_del[node-2]+m_delimiter.length();
 	}
 
-
 	// return the node content
 	if (node==m_nodes){
 		// last node, no need for end pisition in substring
-		return (char*)StS.substring(left,StS.length()).c_str();
+		NodeSelected= StS.substring(left,StS.length());
+
 	}else{
 //		return (char*)StS.substring(m_pos_del[node-1]+1,m_pos_del[node]-1).c_str();
-		return (char*)StS.substring(left,right).c_str();
+		NodeSelected=StS.substring(left,right);
 
 	}
+	return NodeSelected.c_str();
 }
 
--- src/StringD.h
+++ src/StringD.h
@@ -16,26 +16,21 @@
 
 class StringD {
 	public:
-		String m_delimiter; // node m_delimiter for split up of string content
-		//StringD();
-
-
 		StringD(char* StringToParase, const char* Delimiter);
 		StringD(void* StringToParase, const char* Delimiter,int length);
 		~StringD(); // destructor
 		//String Node(int position); // pointer to the
-		void PrintPosDelimiters(); // print the positions of the delimiters
-		int m_nodes; // number of nodes in the string
-		uint8_t m_pos_del[254]; //array of delimiter positions in the string
 		uint8_t max_node(); //getter function for the total number of nodes identified
-		char* get_node(uint8_t node);// function to get a specific node
+		const char* get_node (uint8_t node);// function to get a specific node
+		String NodeSelected; //substring of Sts
 
 	private:
-//		int m_StartPos; // start position of the node in the string
-//		int EndPos; // end position of the node in the string
 		void Process(); // analyse String content
-		String StS; // copy of String content to be analysed
+		String StS; // copy of String content to be analysedv
 		int m_length; // data length to be analysed
+		int m_nodes; // number of nodes in the string
+		uint8_t m_pos_del[254]; //array of delimiter positions in the string
+		String m_delimiter; // node m_delimiter for split up of string content
 
 };
 
